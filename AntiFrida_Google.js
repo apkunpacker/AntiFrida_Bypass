a88f1804aa4e5a9fd2b233784e0ce6d315ff6a89
ApkUnpacker <apkunpacker@gmail.com>
2024-03-10 22:59:36 +0530

    added anti frida code for google
    This code works for anti frida for google integrity protection aka libpairipcore.so.

---
+++ AntiFrida_Google.js
@@ -0,0 +1,16 @@
+/*
+Created By @apkunpacker
+It only bypass anti-frida detection which crash app even after frida-server is closed.
+It won't work if you use it with anti java hook ( Java.use("xyz") )
+Java.use have seperate detections.
+I hate those guy who sells open source scripts
+*/
+Interceptor.attach(Module.findExportByName(null, "strlen"), {
+    onEnter: function(args) {
+        var cmd = args[0].readCString();
+        if (cmd.indexOf("rwxp") !== -1) {          
+            cmd = cmd.replaceAll("rwxp", "r-xp");
+            args[0].writeUtf8String(cmd);
+        }
+    }
+})
